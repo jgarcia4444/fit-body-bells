ec344911ab173bbd620de1542ccc29914f4684bc
Jake Garcia <bballwiz4@gmail.com>
2024-01-22 20:57:05 -0700

    Added a title to the quationnaire and set up the questions array with scaffolded out question objects that will be used to gather information from the user and display the proper inputs.

--- src/Pages/Questionnaire/index.js
+++ src/Pages/Questionnaire/index.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Questionnaire = () => {
 
+    const questions = [
+        {titleMessage: "Hi I'm Breanna Bell, what's you name?", inputs: []},
+        {titleMessage: "Age? (This helps tailor your experience)", inputs: []},
+        {titleMessage: "Contact Info", inputs: []},
+        {titleMessage: "Have you worked with a personal trainer or coach?", inputs: []},
+        {titleMessage: "How long have you been on your fitness journey?", inputs: []},
+        {titleMessage: "", inputs: []},
+        {titleMessage: "", inputs: []},
+        {titleMessage: "", inputs: []},
+        {titleMessage: "", inputs: []},
+        {titleMessage: "", inputs: []}
+    ]
+
     return (
-        <div className="">
-            <h2>Hola</h2>
+        <div className="w-screen h-screen flex flex-col items-center justify-center bg-black text-white">
+            <h1 className="text-3xl ">Starter Questionnaire</h1>
+            <QuestionCard />
         </div>
     )
 }
