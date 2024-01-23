38a136e0707ec5b2c4ef9f53af99777bb5f2766f
Jake Garcia <bballwiz4@gmail.com>
2024-01-22 21:33:48 -0700

    Started to add in information to be used for the inputs.

--- src/Pages/Questionnaire/index.js
+++ src/Pages/Questionnaire/index.js
@@ -1,18 +1,93 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const Questionnaire = () => {
 
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    const inputIconSize = 24;
+    const inputIconColor = "#000";
+
     const questions = [
-        {titleMessage: "Hi I'm Breanna Bell, what's you name?", inputs: []},
-        {titleMessage: "Age? (This helps tailor your experience)", inputs: []},
-        {titleMessage: "Contact Info", inputs: []},
-        {titleMessage: "Have you worked with a personal trainer or coach?", inputs: []},
-        {titleMessage: "How long have you been on your fitness journey?", inputs: []},
-        {titleMessage: "", inputs: []},
-        {titleMessage: "", inputs: []},
-        {titleMessage: "", inputs: []},
-        {titleMessage: "", inputs: []},
-        {titleMessage: "", inputs: []}
+        {
+            titleMessage: "Hi I'm Breanna Bell, what's you name?", 
+            inputs: [
+                {
+                    label: "First", 
+                    placeholder: "first name" , 
+                    icon: "", 
+                    value: "", 
+                    type: "text"
+                }, 
+                {
+                    label: "Last", 
+                    placeholder: "last name" , 
+                    icon: "", 
+                    value: "", 
+                    type: "text"
+                }
+            ]
+        },
+        {
+            titleMessage: "Age? (This helps tailor your experience)", 
+            inputs: [
+                {
+                    label: "Age", 
+                    placeholder: "Age" , 
+                    icon: "", 
+                    value: "", 
+                    type: "number"
+                }
+            ]
+        },
+        {
+            titleMessage: "Contact Info", 
+            inputs: [
+                {
+                    label: "Instagram", 
+                    placeholder: "username" , 
+                    icon: "", 
+                    value: "", 
+                    type: "text"
+                },
+                {
+                    label: "Phone", 
+                    placeholder: "phone number" , 
+                    icon: "", 
+                    value: "", 
+                    type: "text"
+                }
+            ]
+        },
+        {
+            titleMessage: "Have you worked with a personal trainer or coach?", 
+            inputs: [{label: "If yes, to what capacity?", placeholder: "message" , icon: "", value: "", type: "text"},]
+        },
+        {
+            titleMessage: "How long have you been on your fitness journey?", 
+            inputs: [{label: "", placeholder: "Write your answer here..." , icon: "", value: "", type: "text"}]
+        },
+        {
+            titleMessage: "How many times a week do you exercise?", 
+            inputs: [{label: "Days per Week", placeholder: "How many days do you exercise?" , icon: "", value: "", type: "text"}]
+        },
+        {
+            titleMessage: "Food", 
+            inputs: [{label: "What is you relationship with Food?", placeholder: "message" , icon: "", value: "", type: "text"}]
+        },
+        {
+            titleMessage: "Improvement", 
+            inputs: [{label: "What would you like to improve?", placeholder: "message" , icon: "", value: "", type: "text"}]
+        },
+        {
+            titleMessage: "", 
+            inputs: [{label: "", placeholder: "" , icon: "", value: "", type: ""}]
+        },
+        {
+            titleMessage: "", 
+            inputs: [
+                {label: "", placeholder: "" , icon: "", value: "", type: ""}
+            ]
+        }
     ]
 
     return (
