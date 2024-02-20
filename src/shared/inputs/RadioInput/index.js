68236d85a2e84de90f4ec3b9fcef0ca976d8c251
Jake Garcia <bballwiz4@gmail.com>
2024-02-20 12:23:33 -0700

    The radio inputs for has a trainer before now prepopulate based off the hasTrainerBefore redux value.

--- src/shared/inputs/RadioInput/index.js
+++ src/shared/inputs/RadioInput/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const RadioInput = ({label, updateHasTrainer}) => {
+const RadioInput = ({label, updateHasTrainer, hasTrainerBefore}) => {
 
     const radioRowStyle = "flex flex-row items-center";
     const radioButtonStyle = "w-4 h-4"
@@ -13,15 +13,21 @@ const RadioInput = ({label, updateHasTrainer}) => {
             <div className="flex flex-row gap-4">
                 <div className={radioRowStyle}>
                     <label className="mr-2" htmlFor="Yes">Yes</label>
-                    <input className={radioButtonStyle} type="radio" name="trainer" id="" onClick={() => updateHasTrainer(true)} value={true} />
+                    <input checked={hasTrainerBefore === true ? true : false} className={radioButtonStyle} type="radio" name="trainer" id="" onClick={() => updateHasTrainer(true)} value={true} />
                 </div>
                 <div className={radioRowStyle}>
                     <label className="mr-2" htmlFor="No">No</label>
-                    <input className={radioButtonStyle} type="radio" name="trainer" id="" onClick={() => updateHasTrainer(false)} value={false} />
+                    <input checked={(hasTrainerBefore === false || hasTrainerBefore === undefined) ? true : false} className={radioButtonStyle} type="radio" name="trainer" id="" onClick={() => updateHasTrainer(false)} value={false} />
                 </div>
             </div>
         </div>
     )
+}
+
+const mapStateToProps = state => {
+    return {
+        hasTrainerBefore: state.questions.hasTrainerBefore,
+    }
 }
 
 const mapDispatchToProps = dispatch => {
@@ -31,6 +37,6 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(RadioInput);
