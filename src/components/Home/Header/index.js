212df5b9d0b8ce360602031e67ef61d2e92ac69f
Jake Garcia <bballwiz4@gmail.com>
2024-03-09 13:28:02 -0700

    Added in the social medias for instagram and tiktok as part of the header.

--- src/components/Home/Header/index.js
+++ src/components/Home/Header/index.js
@@ -1,11 +1,18 @@
 
 import React from 'react'
 
+import Socials from '../../../shared/Socials';
+
 const Header = () => {
 
     return (
-        <div className="h-56 bg-black w-full flex flex-row items-center justify-center">
-            <h1 className="text-white text-6xl font-thin font-serif">Fit Body Bells</h1>
+        <div className="h-56 bg-black w-full flex flex-col justify-center relative">
+            <div className="w-full">
+                <h1 className="text-white text-6xl font-bold font-serif text-center">Strong AF Fitness</h1>
+            </div>
+            <div className="w-1/4 absolute bottom-2 right-2">
+                <Socials />
+            </div>
         </div>
     )
 }
---
+++ src/shared/Socials/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiInstagram } from "react-icons/fi";
+import TikTokIcon from './TikTokIcon';
+
+const Socials = () => {
+
+    const iconColor = "#fff";
+    const iconSize = 24;
+
+    const iconClassName = "transition-all duration-300 hover:scale-110"
+
+    const instagramIcon = (
+        <a href="https://www.instagram.com/breemariebells?igsh=MTNmbXp10GpidXh5bA==" rel="noreferrer" target='_blank' className={iconClassName}>
+            <FiInstagram color={iconColor} size={iconSize} />
+        </a>
+    )
+
+    const tikTok = (
+        <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@breannabell_?_t=8kXj1BTslxC&_r=1" className={iconClassName}>
+            <TikTokIcon />
+        </a>
+    )
+
+    return (
+        <div className="flex flex-row w-full items-center justify-end gap-2">
+            {instagramIcon}
+            {tikTok}
+        </div>
+    )
+}
+
+export default Socials;
