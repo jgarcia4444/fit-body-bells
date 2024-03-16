32b1bb50bff825962099f8a46a550310f30f0c5b
Jake Garcia <bballwiz4@gmail.com>
2024-03-16 00:21:49 -0600

    gave the questionnaire actions buttons a shadow upon hovering.

--- src/components/Questionnaire/QuestionnaireActionButtons/index.js
+++ src/components/Questionnaire/QuestionnaireActionButtons/index.js
@@ -7,7 +7,7 @@ const QuestionnaireActionButtons = ({nextPress, previousPress, showSubmit}) => {
     const iconColor = "#fff";
     const iconSize = 24
 
-    const iconButtonStyle = `p-2 bg-black rounded-full hover:scale-110 hover:bg-yellow-500 hover:cursor-pointer transition-all duration-300`
+    const iconButtonStyle = `p-2 bg-black rounded-full hover:scale-110 hover:bg-yellow-500 hover:cursor-pointer hover:shadow-black hover:shadow active:shadow-none transition-all duration-300`
 
     const nextButton = (
         <div onClick={nextPress} className={iconButtonStyle}>
