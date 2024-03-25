fb502587a4b8ed4c4dfca6ba9693bff0199f02d6
Jake Garcia <bballwiz4@gmail.com>
2024-03-25 00:16:11 -0600

    Gave the entrance of the hompage an appearnce and growing in size animation.

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import Header from '../../components/Home/Header'
 import About from '../../components/Home/About';
 import HomeBottomCTA from '../../components/Home/HomeBottomCTA';
 
 const Home = () => {
+
+    const [viewOpacity, setViewOpacity] = useState("opacity-0 scale-0")
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            setViewOpacity("opacity-100 scale-100")
+        }, 200);
+    },[])
+
     return (
-        <div className="h-screen w-screen flex flex-row flex-wrap items-center justify-center">
+        <div className={`h-screen w-screen flex flex-row flex-wrap items-center justify-center duration-1000 transition-all ${viewOpacity}`}>
             <Header />
             <About />
             <HomeBottomCTA />
